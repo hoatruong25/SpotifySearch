a7a4da3888bc337335d882d3f14d32e63cc38933
thuanhoa <thuanhoa.dev@zim.vn>
2025-08-10 19:17:16 +0700

    feature: register elastic service

--- SpotifySearch.API/Program.cs
+++ SpotifySearch.API/Program.cs
@@ -1,3 +1,5 @@
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
 using Scalar.AspNetCore;
 using SpotifySearchAPI.BusinessService.LoadDataService;
 using SpotifySearchAPI.Repository;
@@ -7,11 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+/* Register Elasticsearch service */
+var elasticUri = builder.Configuration["Elasticsearch:Uri"];
+var username   = builder.Configuration["Elasticsearch:Username"];
+var password   = builder.Configuration["Elasticsearch:Password"];
+var settings = new ElasticsearchClientSettings(new Uri(elasticUri))
+    .Authentication(new BasicAuthentication(username, password))
+    .DefaultIndex("default-index") // optional
+    .PrettyJson()
+    .RequestTimeout(TimeSpan.FromMinutes(2));
+
 /* DI Business Service */
 builder.Services.AddScoped<ILoadDataService, LoadDataService>();
 
 /* DI Repository */
 builder.Services.AddScoped<ISpotifyTrackRepository, SpotifyTrackRepository>();
+
+/* DI 3rd party service */
+builder.Services.AddSingleton(new ElasticsearchClient(settings));
 
 var app = builder.Build();
 
